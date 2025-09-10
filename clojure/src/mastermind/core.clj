2be0cfd79212b2a4080d3b1c71d79c603f270b73
Fabio 'c0m3tx' Vitale <cometspk@gmail.com>
2025-09-10 22:17:06 +0200

    feat: completed basic implementation

--- clojure/src/mastermind/core.clj
+++ clojure/src/mastermind/core.clj
@@ -1,16 +1,35 @@
 (ns mastermind.core
-  (:gen-class))
+  (:gen-class)
+  (:use [clojure.set :as set :only (intersection)]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn generate-code [] (->> (range 0 9) (vec) (shuffle) (take 4) (vec)))
 
-(defn generate-code [] (->> (range 0 9) (vec) (shuffle) (take 4)))
+(defn valid? [code] (= 4 (count (set code))))
 
 (defn user-input []
   (println "Enter a 4 digit code:")
   (let [code (->> (read-line) (map #(Character/digit %1 10)) (filter (comp not neg?)))]
-    (if (and (= 4 (count code)) (= (dedupe (sort code)) (sort code)))
-      code
-      nil)))
+    (if (valid? code) code nil)))
+
+(defn xes [code guess] (count (filter identity (map == code guess))))
+(defn oes [code guess] (- (count (set/intersection (set code) (set guess))) (xes code guess)))
+
+(defn play-loop [secret attempts]
+  (println "You have" attempts "attempts")
+  (let [guess (user-input) xs (xes secret guess) os (oes secret guess)]
+    (cond 
+      (nil? guess) (do (println "Invalid input, enter 4 distinct digits.") (recur secret attempts)) 
+      (= 4 xs) (println "You won!")
+      (= 1 attempts) (println "You lost, the code was" (str secret))
+      :else (do 
+              (println "Result:" (apply str (concat (repeat xs "X") (repeat os "O")))) 
+              (recur secret (dec attempts)))
+    )))
+
+(defn -main
+  [& args]
+  (let [secret (generate-code)]
+    (println "Welcome to Mastermind, the code is" (str secret) "for debugging purposes")
+    (play-loop secret 10)
+  ))
+
--- clojure/test/mastermind/core_test.clj
+++ clojure/test/mastermind/core_test.clj
@@ -6,11 +6,16 @@
   (testing "Nah I'm good"
     (is (= 0 0))))
 
+(deftest valid-test
+  (testing "valid verifies that the code has the right length and no dups"
+    (is (valid? [1 2 3 4]))
+    (is (not (valid? [1 2 3])))
+    (is (not (valid? [1 1 3 4])))))
+
 (deftest generate-code-test
   (testing "generate code generates a random value of 4 digits"
     (let [code (generate-code)]
-      (is (= 4 (count code)))
-      (is (= (dedupe (sort code)) (sort code))))))
+      (is (valid? code)))))
 
 (deftest user-input-test
   (testing "user input accepts valid input"
@@ -29,3 +34,21 @@
     (with-redefs [read-line (fn [] "1a345"), println (fn [& _] nil)]
       (let [code (user-input)]
         (is (= code [1 3 4 5]))))))
+
+(deftest xes-test
+  (testing "xes are calculated correctly"
+    (is (= 4 (xes [1 2 3 4] [1 2 3 4])))
+    (is (= 3 (xes [1 2 3 4] [1 2 3 5])))
+    (is (= 2 (xes [1 2 3 4] [2 1 3 4])))
+    (is (= 1 (xes [1 2 3 4] [1 5 6 7])))
+    (is (= 0 (xes [1 2 3 4] [5 6 7 8])))
+    ))
+
+(deftest oes-test
+  (testing "oes are calculated correctly"
+    (is (= 0 (oes [1 2 3 4] [5 2 6 7])))
+    (is (= 1 (oes [1 2 3 4] [2 5 6 7])))
+    (is (= 2 (oes [1 2 3 4] [2 1 3 4])))
+    (is (= 3 (oes [1 2 3 4] [2 3 1 4])))
+    (is (= 4 (oes [1 2 3 4] [2 1 4 3])))
+    ))
