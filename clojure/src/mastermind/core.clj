7138261e2928fb286ead65bdb28919f8b661fe5c
Fabio Vitale <fabio.vitale@helloprima.com>
2025-09-10 14:37:46 +0200

    feat: add user input parsing

--- clojure/src/mastermind/core.clj
+++ clojure/src/mastermind/core.clj
@@ -7,3 +7,10 @@
   (println "Hello, World!"))
 
 (defn generate-code [] (->> (range 0 9) (vec) (shuffle) (take 4)))
+
+(defn user-input []
+  (println "Enter a 4 digit code:")
+  (let [code (->> (read-line) (map #(Character/digit %1 10)) (filter (comp not neg?)))]
+    (if (and (= 4 (count code)) (= (dedupe (sort code)) (sort code)))
+      code
+      nil)))
--- clojure/test/mastermind/core_test.clj
+++ clojure/test/mastermind/core_test.clj
@@ -9,6 +9,23 @@
 (deftest generate-code-test
   (testing "generate code generates a random value of 4 digits"
     (let [code (generate-code)]
-      (println code)
       (is (= 4 (count code)))
       (is (= (dedupe (sort code)) (sort code))))))
+
+(deftest user-input-test
+  (testing "user input accepts valid input"
+    (with-redefs [read-line (fn [] "1234"), println (fn [& _] nil)]
+      (let [code (user-input)]
+        (is (= code [1 2 3 4]))))
+    (with-redefs [read-line (fn [] "1123"), println (fn [& _] nil)]
+      (let [code (user-input)]
+        (is (nil? code))))
+    (with-redefs [read-line (fn [] "123"), println (fn [& _] nil)]
+      (let [code (user-input)]
+        (is (nil? code))))
+    (with-redefs [read-line (fn [] "12a4"), println (fn [& _] nil)]
+      (let [code (user-input)]
+        (is (nil? code))))
+    (with-redefs [read-line (fn [] "1a345"), println (fn [& _] nil)]
+      (let [code (user-input)]
+        (is (= code [1 3 4 5]))))))
