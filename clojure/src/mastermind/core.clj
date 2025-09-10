17bacb6dc658cdfb284abccbf5622543d739cfd3
Fabio 'c0m3tx' Vitale <cometspk@gmail.com>
2025-09-10 22:35:53 +0200

    refactor: simplify the code a bit

--- clojure/src/mastermind/core.clj
+++ clojure/src/mastermind/core.clj
@@ -7,29 +7,32 @@
 (defn valid? [code] (= 4 (count (set code))))
 
 (defn user-input []
-  (println "Enter a 4 digit code:")
+  (print "Enter a 4 digit code: ")
+  (flush)
   (let [code (->> (read-line) (map #(Character/digit %1 10)) (filter (comp not neg?)))]
     (if (valid? code) code nil)))
 
 (defn xes [code guess] (count (filter identity (map == code guess))))
 (defn oes [code guess] (- (count (set/intersection (set code) (set guess))) (xes code guess)))
 
+(defn output-result [xs os] (println "Result:" (apply str (concat (repeat xs "X") (repeat os "O")))))
+
 (defn play-loop [secret attempts]
-  (println "You have" attempts "attempts")
+  (print "You have" attempts "attempts left. ")
   (let [guess (user-input) xs (xes secret guess) os (oes secret guess)]
     (cond 
       (nil? guess) (do (println "Invalid input, enter 4 distinct digits.") (recur secret attempts)) 
       (= 4 xs) (println "You won!")
       (= 1 attempts) (println "You lost, the code was" (str secret))
       :else (do 
-              (println "Result:" (apply str (concat (repeat xs "X") (repeat os "O")))) 
+              (output-result xs os) 
               (recur secret (dec attempts)))
     )))
 
 (defn -main
   [& args]
   (let [secret (generate-code)]
-    (println "Welcome to Mastermind, the code is" (str secret) "for debugging purposes")
+    (println "Welcome to Mastermind!")
     (play-loop secret 10)
   ))
 
